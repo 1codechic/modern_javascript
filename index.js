9847bc60ba3f79638e896d9ac7f869f9f23aaf01
1codechic <1codechic@devchics.com>
2019-04-01 21:41:50 -0400

    added babel and updated index.js with additional test js code for testing

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ var moment = require ('moment');
 console.log("Hey!!! I am from the index.js file");
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+// Interpolate variable bindings
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
+
+
+function square() {
+  let example = () => {
+    let numbers = [];
+    for (let number of arguments) {
+      numbers.push(number * number);
+    }
+
+    return numbers;
+  };
+
+  return example();
+}
+
+console.log(square(2, 4, 7.5, 8, 11.5, 21)); // returns: [4, 16, 56.25, 64, 132.25, 441]
